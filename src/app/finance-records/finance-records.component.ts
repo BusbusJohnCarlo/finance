88458cd770d2bbaec22a4da99262eaf3170b2863
BusbusJohnCarlo <78535779+BusbusJohnCarlo@users.noreply.github.com>
2021-04-13 16:47:47 +0800

    v1

--- src/app/finance-records/finance-records.component.ts
+++ src/app/finance-records/finance-records.component.ts
@@ -13,13 +13,13 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', symbols:'A' },
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' , symbols:'A'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' , symbols:'A'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' , symbols:'A'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B' , symbols:'A'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' , symbols:'M'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' , symbols:'L'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' , symbols:'O'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B' , symbols:'G'},
   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', symbols:'A'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', symbols:'A'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', symbols:'A'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', symbols:'G'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', symbols:'O'},
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', symbols:'A'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', symbols:'A'},
 ];
